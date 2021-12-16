11f020378d0fd32c1e69dcbe205fe38eb9cc13cb
Edzel Pasamanero <edzel.pasamanero@gmail.com>
2021-12-17 00:49:11 +0800

    Solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import {
-  defaultSkinColor,
-  defaultClothesColor,
-} from '../../constants';
+import { defaultSkinColor, defaultClothesColor } from '../../constants';
 import Character from '../Character';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import ControlPane from '../ControlPane';
@@ -24,9 +21,7 @@ function App() {
   const [face, setFace] = React.useState(0);
   const [accessory, setAccessory] = React.useState(0);
   const [skinColor, setSkinColor] = React.useState(defaultSkinColor);
-  const [clothesColor, setClothesColor] = React.useState(
-    defaultClothesColor
-  );
+  const [clothesColor, setClothesColor] = React.useState(defaultClothesColor);
 
   return (
     <main className={styles.characterEditor}>
@@ -34,43 +29,43 @@ function App() {
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the
-            controls below. What sort of adventure will you embark on?{' '}
+            Customize your character's look and style using the controls below.
+            What sort of adventure will you embark on?{' '}
           </p>
         </header>
         <div className={styles.controlColumn}>
           <ControlPane
-            title="Bodies"
+            title='Bodies'
             options={bodyOptions}
             currentOption={body}
             handleSelectOption={setBody}
           />
           <ControlPane
-            title="Heads"
+            title='Heads'
             options={headOptions}
             currentOption={head}
             handleSelectOption={setHead}
           />
           <ControlPane
-            title="Faces"
+            title='Faces'
             options={faceOptions}
             currentOption={face}
             handleSelectOption={setFace}
           />
           <ControlPane
-            title="Accessories"
+            title='Accessories'
             options={accessoryOptions}
             currentOption={accessory}
             handleSelectOption={setAccessory}
           />
           <ControlPane
-            title="Skin Color"
+            title='Skin Color'
             options={skinColorOptions}
             currentOption={skinColor}
             handleSelectOption={setSkinColor}
           />
           <ControlPane
-            title="Clothing Color"
+            title='Clothing Color'
             options={clothesColorOptions}
             currentOption={clothesColor}
             handleSelectOption={setClothesColor}
